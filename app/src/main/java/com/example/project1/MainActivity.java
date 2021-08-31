81bb9b697f96ce47b24c6332e2c539cd5ae4937e
arsalaerfan <erfanarsala831@gmail.com>
2021-08-31 10:25:02 -0700

    Working on main login Page. Logic behind login button

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -2,22 +2,49 @@ package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText etName;
-    private EditText etPassword;
-    private Button btnLogin;
+    private EditText eName;
+    private EditText ePassword;
+    private Button eLogin;
 
-    etName = findViewById(R.id.etName);
-    etPassword = findViewById(R.id.etPassword);
+    String userName = "admin";
+    String userPassword = "admin";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        eName = findViewById(R.id.etName);
+        ePassword = findViewById(R.id.etPassword);
+        eLogin = findViewById(R.id.btnLogin);
+
+        eLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userName = eName.getText().toString();
+                userPassword = ePassword.getText().toString();
+
+                if(userName.isEmpty() || userPassword.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Please enter name and password!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 }
