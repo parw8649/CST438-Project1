51e133132f4b200905e7a71af0b5540e6774f6cc
arsalaerfan <erfanarsala831@gmail.com>
2021-08-31 10:16:04 -0700

    Working on main login Page. Have a username and password and check validity.

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etName;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    etName = findViewById(R.id.etName);
+    etPassword = findViewById(R.id.etPassword);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
