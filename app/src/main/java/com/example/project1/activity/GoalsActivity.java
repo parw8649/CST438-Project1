16acf4f89050a46801d0725b4a0157223256516c
Prince Rios <riosprince7@gmail.com>
2021-09-17 15:47:27 -0700

    Added functionality to return to the main activity after user clicks back inside the goals activity

--- app/src/main/java/com/example/project1/activity/GoalsActivity.java
+++ app/src/main/java/com/example/project1/activity/GoalsActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.project1.R;
 
@@ -12,6 +13,8 @@ public class GoalsActivity extends AppCompatActivity {
 
     private static final String USER_ID_KEY = "com.example.project1.userIdKey";
     private static final String PREFS = "com.example.project1.prefs";
+    private int mUserId = -1;
+    Button btnBack;
 
     //good
 
@@ -19,11 +22,22 @@ public class GoalsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals);
+
+        wireUpDisplay();
     }
 
     public static Intent intentFactory(Context context, int mUserId) {
         Intent intent = new Intent(context, GoalsActivity.class);
         intent.putExtra(USER_ID_KEY, mUserId);
         return intent;
+    }
+
+    public void wireUpDisplay() {
+        btnBack = findViewById(R.id.btn_activity_goals_main_back);
+
+        btnBack.setOnClickListener(v -> {
+            Intent intent = MainActivity.intentFactory(this, mUserId);
+            startActivity(intent);
+        });
     }
 }
