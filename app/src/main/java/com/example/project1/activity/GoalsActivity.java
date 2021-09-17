43cd1b4b4e62959b6263bff579254b0a250a0d85
thekokoloko <sotoramiro30@yahoo.com>
2021-09-17 03:43:53 -0700

    working on goals activity

---
+++ app/src/main/java/com/example/project1/activity/GoalsActivity.java
@@ -0,0 +1,16 @@
+package com.example.project1.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project1.R;
+
+public class GoalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goals);
+    }
+}
